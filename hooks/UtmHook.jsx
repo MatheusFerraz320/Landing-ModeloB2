b4e7f7c3e00ad2323b927ba9f09366e10bd60630
matheusb2 <matheusaugustosabinoferraz@outlook.com>
2026-03-17 13:18:50 -0300

    feat:adição do ad_id no hook Utm

--- hooks/UtmHook.jsx
+++ hooks/UtmHook.jsx
@@ -8,16 +8,17 @@ export function UtmHook() {
 
     const urlParams = new URLSearchParams(window.location.search);
     const params = {
-      utm_source: urlParams.get('utm_source') || '',
-      utm_medium: urlParams.get('utm_medium') || '',
-      utm_campaign: urlParams.get('utm_campaign') || '',
-      utm_term: urlParams.get('utm_term') || '',
-      utm_content: urlParams.get('utm_content') || '',
+      ad_id: urlParams.get('ad_id') || 'Erro: ad_id não encontrado',
+      utm_source: urlParams.get('utm_source') || 'Erro: utm_source não encontrado',
+      utm_medium: urlParams.get('utm_medium') || 'Erro: utm_medium não encontrado',
+      utm_campaign: urlParams.get('utm_campaign') || 'Erro: utm_campaign não encontrado',
+      utm_term: urlParams.get('utm_term') || 'Erro: utm_term não encontrado',
+      utm_content: urlParams.get('utm_content') || 'Erro: utm_content não encontrado',
     };
 
     setUtmParams(params);
     
-    // usar modalForm
+    // salvar no navegador para persistencia
     sessionStorage.setItem('utm_params', JSON.stringify(params));
   }, []);
 
