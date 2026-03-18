9eb18fd58a5840597b3ad462979fe01a583bba43
Matheus Augusto Sabino Ferraz <matheusaugustosabinoferraz@outlook.com>
2026-03-18 17:17:32 +0000

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         {/* GTM: script principal */}
         <Script
           id="gtm-script"
-          strategy="afterInteractive" // ⚡ não bloqueia LCP
+          strategy="beforeInteractive" // ⚡ não bloqueia LCP
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
