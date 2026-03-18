05f725b3895f9cc273ae5593ede41ba942b63c13
Matheus Augusto Sabino Ferraz <matheusaugustosabinoferraz@outlook.com>
2026-03-18 17:10:10 +0000

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -3,17 +3,19 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head>
-        {/* Google Tag Manager */}
-      <Script
-  id="gtm"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `(function(w,d,s,l,i){...})(window,document,'script','dataLayer','GTM-WZPD6JC');`,
-  }}
-/>
-      </Head>
-
+<Head>
+  <Script
+    id="gtm-script"
+    strategy="beforeInteractive" //
+    dangerouslySetInnerHTML={{
+      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WZPD6JC');`,
+    }}
+  />
+</Head>
       <body className="antialiased">
         {/* Google Tag Manager (noscript) */}
         <noscript>
