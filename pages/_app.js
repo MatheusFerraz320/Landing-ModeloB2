f2f5b3d759a9ed7fa88bb21c32b0efe227ed2cc7
Matheus Ferraz <matheusaugustosabinoferraz@outlook.com>
2026-03-18 19:25:35 -0300

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,46 @@
 import "@/styles/globals.css";
-
 import Script from "next/script";
-
 import { LazyMotion, domAnimation } from "framer-motion";
 
-export function reportWebVitals(metric) {
-  if (typeof window !== "undefined" && typeof window.gtag === "function") {
-    window.gtag("event", metric.name, {
-      event_category: "Web Vitals",
-      value: Math.round(metric.value),
-      event_label: metric.id,
-      non_interaction: true,
-    });
-  }
-}
+
 
 export default function App({ Component, pageProps }) {
   return (
     <LazyMotion features={domAnimation} strict>
-      {/*GTM*/}
+      {/* GTM principal - Carregamento via interação para não bloquear o Lighthouse */}
       <Script
         id="gtm-script"
-        strategy="lazyOnload" 
+        strategy="afterInteractive" 
         dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){
-            w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s);
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i;
-            f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-WZPD6JC');`,
+          __html: `
+            let gtmLoaded = false;
+            function loadGTM() {
+              if (gtmLoaded) return;
+              gtmLoaded = true;
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s);
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WZPD6JC');
+              
+              // Remove events once loaded
+              window.removeEventListener('scroll', loadGTM);
+              window.removeEventListener('mousemove', loadGTM);
+              window.removeEventListener('touchstart', loadGTM);
+            }
+            
+            // Load on interaction to protect Lighthouse scores
+            window.addEventListener('scroll', loadGTM, { passive: true });
+            window.addEventListener('mousemove', loadGTM, { passive: true });
+            window.addEventListener('touchstart', loadGTM, { passive: true });
+            
+            // Fallback load after 5 seconds if no interaction
+            setTimeout(loadGTM, 5000);
+          `,
         }}
       />
 
