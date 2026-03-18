67a023933fd94344112011a9262b313436b5bd3e
Matheus Augusto Sabino Ferraz <matheusaugustosabinoferraz@outlook.com>
2026-03-18 12:54:33 +0000

    fix:tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import script from "next/script";
 
 export default function Document() {
   const gtmId = process.env.NEXT_PUBLIC_GTM_ID || "GTM-WZPD6JC";
 
   return (
     <Html lang="pt-BR">
-      <Head>  
- <Head>
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${gtmId}');`,
-    }}
-  />
-</Head>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${gtmId}');`,
+          }}
+        />
       </Head>
+
       <body className="antialiased">
         <noscript>
           <iframe
